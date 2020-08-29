249f0dabaed39860de853fb5a03f9bb325276f04
Marcelo Cubillos <marceloclubhouse@gmail.com>
2020-08-28 18:16:36 -0700

    Updated functions to camelcase

--- php/includes.php
+++ php/includes.php
@@ -14,17 +14,17 @@
  *  for more information.
  */
 
-function essentials_include_libsyn() {
+function essentialsIncludeLibsyn() {
     /* Include scripts and styles for Libsyn podcast feeds */
     wp_enqueue_script( 'essentials-libsyn-js', plugins_url( '../js/embed-libsyn.js', __FILE__ ));
     wp_enqueue_style( 'essentials-libsyn-css', plugins_url( '../css/embed-libsyn.css', __FILE__ ));
 }
-add_action('wp_enqueue_scripts', 'essentials_include_libsyn');
+add_action('wp_enqueue_scripts', 'essentialsIncludeLibsyn');
 
 
-function essentials_include_youtube_playlist() {
+function essentialsIncludeYoutubePlaylist() {
     /* Include scripts and styles for embedding YouTube playlists */
     wp_enqueue_script( 'essentials-youtube-playlist-js', plugins_url( '../js/embed-youtube-playlist.js', __FILE__ ));
     wp_enqueue_style( 'essentials-youtube-playlist-css', plugins_url( '../css/embed-youtube-playlist.css', __FILE__ ));
 }
-add_action('wp_enqueue_scripts', 'essentials_include_youtube_playlist');
+add_action('wp_enqueue_scripts', 'essentialsIncludeYoutubePlaylist');
